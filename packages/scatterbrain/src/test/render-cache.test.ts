86a2ee639ab0400b9c0756d2f18530e721de2979
noah <noah.shepard@alleninstitute.org>
2024-04-10 10:46:40 -0700

    test some more edgy cases of this separate-but-related cache system

--- packages/scatterbrain/src/test/render-cache.test.ts
+++ packages/scatterbrain/src/test/render-cache.test.ts
@@ -116,10 +116,9 @@ describe('async cache', () => {
 
 
             mockPromises.mockResolve(spies[1])
-            // resolve one so that we can be sure its value lives in the cache
+            // resolve one first so that we can be sure its value lives in the cache
             await Promise.resolve()
             expect(cache.isCached(toCacheKey('position'))).toBeTruthy()
-            console.dir(cache)
             // then reject the other to fail the overall task
             mockPromises.mockReject(spies[0], 'fetch failed for reals')
             await Promise.resolve()
@@ -130,6 +129,126 @@ describe('async cache', () => {
             expect(boom).toBeTruthy()
             // the one that failed of course is missing from the cache
             expect(cache.isCached(toCacheKey('color'))).toBeFalsy()
+        })
+        it('evicts data after the soft limit is hit', async () => {
+            // each of our tasks requests two chunks of data
+            // the cache has a limit of 10 items (see beforeEach)
+            let allKeysSoFar: string[] = []
+            for (let i = 0; i < 5; i++) {
+                const { fetchers, id, spies } = fetchFakeItem(i, [255, 0, i], [1, 2 * i, 3 * i])
+                const toCacheKey = partial(cacheKey, { id })
+                const result = cache.cacheAndUse(fetchers, render, toCacheKey)
+                // store all the cache keys so we can expect them later
+                allKeysSoFar.push(toCacheKey('color'), toCacheKey('position'))
+                await resolveFakePromises(spies)
+            }
+            // so after making 5 requests, we should have a full cache
+            expect(cache.areKeysAllCached(allKeysSoFar)).toBe(true);
+            expect(cache.getNumPendingTasks()).toBe(0);
+            // now request a new thing - this should cause the cache to remove at least one item
+            const { fetchers, id, spies } = fetchFakeItem(99, [99, 99, 99], [9, 9, 9])
+            const toCacheKey = partial(cacheKey, { id })
+            const result = cache.cacheAndUse(fetchers, render, toCacheKey);
+            await resolveFakePromises(spies)
+            // we would expect the items with id=0 to have been removed, as they are the oldest
+            expect(cache.isCached(toCacheKey('color'))).toBeTruthy()
+            expect(cache.isCached(toCacheKey('position'))).toBeTruthy()
+            // and the old ones:
+            expect(cache.isCached(cacheKey({ id: 0 }, 'color'))).toBeFalsy()
+            expect(cache.isCached(cacheKey({ id: 0 }, 'position'))).toBeFalsy()
+        })
+        it('does not evict data if it is marked as critical for a pending request', async () => {
+            // almost the same scenario as the previous test, except we're going to intentionally NOT
+            // resolve the task with id=0. Because its still pending, we would like to see that it does not get evicted!
+
+            // each of our tasks requests two chunks of data
+            // the cache has a limit of 10 items (see beforeEach)
+            let allKeysSoFar: string[] = []
+            let reallySlowRequest: Promise<unknown>;
+            for (let i = 0; i < 6; i++) {
+                const { fetchers, id, spies } = fetchFakeItem(i, [255, 0, i], [1, 2 * i, 3 * i])
+                const toCacheKey = partial(cacheKey, { id })
+                const result = cache.cacheAndUse(fetchers, render, toCacheKey)
+                if (i !== 0) {
+                    // dont resolve the stuff for the first task!
+                    allKeysSoFar.push(toCacheKey('color'), toCacheKey('position'))
+                    await resolveFakePromises(spies)
+                } else {
+                    allKeysSoFar.push(toCacheKey('color'))
+                    await resolveFakePromises([spies[0]])
+                    reallySlowRequest = spies[1];
+                    // intentionally not resolving position!
+                }
+            }
+            // so after making 5 requests, we should have an almost full cache
+            expect(cache.areKeysAllCached(allKeysSoFar)).toBe(true);
+            expect(cache.getNumPendingTasks()).toBe(1); // the first task is still pending, because we didn't resolve its position request
+
+            // now request a new thing - this should cause the cache to remove at least one item
+            const { fetchers, id, spies } = fetchFakeItem(99, [99, 99, 99], [9, 9, 9])
+
+            const result = cache.cacheAndUse(fetchers, render, partial(cacheKey, { id }));
+            await resolveFakePromises(spies)
+
+            expect(cache.isCached(cacheKey({ id: 99 }, 'color'))).toBeTruthy()
+            expect(cache.isCached(cacheKey({ id: 99 }, 'position'))).toBeTruthy()
+
+            // we would expect the items with id=0 to have been removed, as they are the oldest, HOWEVER
+            // we never resolved (position,id:0) so we expect to not have removed 
+            expect(cache.isCached(cacheKey({ id: 0 }, 'color'))).toBeTruthy() // its still in the cache
+            expect(cache.isCached(cacheKey({ id: 0 }, 'position'))).toBeFalsy() // its never been resolved, so it cant be in the cache (yet)
+
+            expect(cache.isCached(cacheKey({ id: 1 }, 'color'))).toBeFalsy()
+            expect(cache.isCached(cacheKey({ id: 1 }, 'position'))).toBeFalsy()
+
+            // finally, resolve that first stalled request, and observe us fulfilling our first render task:
+            await resolveFakePromises([reallySlowRequest!])
+            expect(cache.getNumPendingTasks()).toBe(0)
+            // expect everything to have been rendered, regardless of all this cache nonsense
+            expect(rendered.length).toEqual(7) // id=0...5, 99
+            // spot check the last two rendered things:
+            rendered[rendered.length - 2].data.color[1] = 99
+            rendered[rendered.length - 1].data.position[2] = 0 // 3*(i=0)
+        })
+        it('evicts data after the soft limit is hit, while prioritizing Least-recently used entries', async () => {
+            // each of our tasks requests two chunks of data
+            // the cache has a limit of 10 items (see beforeEach)
+            let allKeysSoFar: string[] = []
+            let first: ReturnType<typeof fetchFakeItem> & { toCacheKey: (rq: Columns) => string }
+            for (let i = 0; i < 5; i++) {
+                const { fetchers, id, spies } = fetchFakeItem(i, [255, 0, i], [1, 2 * i, 3 * i])
+                const toCacheKey = partial(cacheKey, { id })
+                if (i === 0) {
+                    first = { fetchers, id, spies, toCacheKey }
+                }
+                const result = cache.cacheAndUse(fetchers, render, toCacheKey)
+                // store all the cache keys so we can expect them later
+                allKeysSoFar.push(toCacheKey('color'), toCacheKey('position'))
+                await resolveFakePromises(spies)
+            }
+            // so after making 5 requests, we should have a full cache
+            expect(cache.areKeysAllCached(allKeysSoFar)).toBe(true);
+            expect(cache.getNumPendingTasks()).toBe(0);
+            // now - re-request the first task (pretend to 'render' it again)
+            const again = first!; // renaming first as again to avoid many copies of the ! operator
+            cache.cacheAndUse(again.fetchers, render, again.toCacheKey);
+            await resolveFakePromises(again.spies);
+            expect(cache.getNumPendingTasks()).toBe(0);
+            // now request a new thing - this should cause the cache to remove at least one item
+            const { fetchers, id, spies } = fetchFakeItem(99, [99, 99, 99], [9, 9, 9])
+            const toCacheKey = partial(cacheKey, { id })
+            const result = cache.cacheAndUse(fetchers, render, toCacheKey);
+            await resolveFakePromises(spies)
+            // we would expect the items with id=1 to have been removed (not id=0 because we re-requested it)
+            expect(cache.isCached(toCacheKey('color'))).toBeTruthy()
+            expect(cache.isCached(toCacheKey('position'))).toBeTruthy()
+
+            expect(cache.isCached(cacheKey({ id: 0 }, 'color'))).toBeTruthy()
+            expect(cache.isCached(cacheKey({ id: 0 }, 'position'))).toBeTruthy()
+            // these are gone - they would have been the second oldest (and thus safe) - but we re-requested the id=0 items
+            // so they get updated as having been used most-recently - the idea being they'll be requested again and maybe other things would be better to evict
+            expect(cache.isCached(cacheKey({ id: 1 }, 'color'))).toBeFalsy()
+            expect(cache.isCached(cacheKey({ id: 1 }, 'position'))).toBeFalsy()
         })
     })
 })
