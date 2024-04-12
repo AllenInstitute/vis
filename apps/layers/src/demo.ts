a62b18afd57a6f8bf6e770c1675a1b7bf7b68123
noah <noah.shepard@alleninstitute.org>
2024-04-12 13:35:23 -0700

    first step to layered rendering: one layer!

--- apps/layers/src/demo.ts
+++ apps/layers/src/demo.ts
@@ -1,10 +1,18 @@
-import { Box2D, type box2D, type vec2 } from "@alleninstitute/vis-geometry";
+import { Box2D, Vec2, type box2D, type vec2 } from "@alleninstitute/vis-geometry";
 import type { Dataset } from "~/loaders/scatterplot/data";
-import type { ColumnData } from "~/loaders/scatterplot/scatterbrain-loader";
+import { loadDataset, type ColumnData, type ColumnarMetadata } from "~/loaders/scatterplot/scatterbrain-loader";
 import REGL from "regl";
 import { AsyncDataCache, type FrameLifecycle, type NormalStatus } from "@alleninstitute/vis-scatterbrain";
+import { ScLayer } from "./scatterplotLayer";
+import { buildRenderer } from "../../scatterplot/src/renderer";
+import { buildImageRenderer } from "../../omezarr-viewer/src/image-renderer";
 const KB = 1000;
 const MB = 1000 * KB;
+
+async function loadJSON(url: string) {
+    // obviously, we should check or something
+    return fetch(url).then(stuff => stuff.json() as unknown as ColumnarMetadata)
+}
 
 
 class Demo {
@@ -19,32 +27,100 @@ class Demo {
     mousePos: vec2;
     pointCache: AsyncDataCache<string, string, ColumnData>;
     textureCache: AsyncDataCache<string, string, REGL.Texture2D>;
-    layers: Layer<typeof this.camera>[]
-    constructor(canvas: HTMLCanvasElement, regl: REGL.Regl) {
+    layer: ScLayer | null;
+    imgRenderer: ReturnType<typeof buildImageRenderer>;
+    private refreshRequested: number = 0;
+    constructor(canvas: HTMLCanvasElement, regl: REGL.Regl, url: string) {
         this.canvas = canvas;
         this.mouse = 'up'
         this.regl = regl;
         this.mousePos = [0, 0]
-        this.layers = [];
+        this.layer = null;
+        const plotRenderer = buildRenderer(regl);
+        this.imgRenderer = buildImageRenderer(regl);
+        this.refreshRequested = 0;
         const [w, h] = [canvas.clientWidth, canvas.clientHeight];
         this.camera = {
             view: Box2D.create([0, 0], [(10 * w) / h, 10]),
             screen: [w, h]
         }
+        this.initHandlers(canvas);
         this.pointCache = new AsyncDataCache<string, string, ColumnData>((_data) => {
             // no op destroyer - GC will clean up for us
         }, (data: ColumnData) => data.data.byteLength, 500 * MB);
         this.textureCache = new AsyncDataCache<string, string, REGL.Texture2D>((d: REGL.Texture2D) => {
             d.destroy()
         }, (_d) => 1, 512)
+
+        loadJSON(url).then((metadata) => {
+            this.dataset = loadDataset(metadata, url);
+            this.layer = new ScLayer(regl, this.pointCache, this.dataset, [w, h], this.imgRenderer, plotRenderer, () => {
+                this.requestReRender();
+            })
+        })
+    }
+    private onCameraChanged() {
+        if (this.layer) {
+            this.layer.onChangeView(this.camera.view);
+        }
+    }
+    requestReRender() {
+        if (this.refreshRequested === 0) {
+            this.refreshRequested = window.requestAnimationFrame(() => {
+                this.refreshScreen();
+                this.refreshRequested = 0;
+            })
+        }
+    }
+    mouseButton(click: "up" | "down") {
+        this.mouse = click;
+    }
+    mouseMove(delta: vec2) {
+        if (this.mouse === "down") {
+            // drag the view
+            const { screen, view } = this.camera;
+            const p = Vec2.div(delta, [this.canvas.clientWidth, this.canvas.clientHeight]);
+            const c = Vec2.mul(p, Box2D.size(view));
+            this.camera = { view: Box2D.translate(view, c), screen };
+            this.onCameraChanged();
+        }
+        this.mousePos = Vec2.add(this.mousePos, delta);
+    }
+    zoom(scale: number) {
+        const { view, screen } = this.camera;
+        const m = Box2D.midpoint(view);
+        this.camera = {
+            view: Box2D.translate(Box2D.scale(Box2D.translate(view, Vec2.scale(m, -1)), [scale, scale]), m),
+            screen,
+        };
+        this.onCameraChanged();
+    }
+    private initHandlers(canvas: HTMLCanvasElement) {
+        canvas.onmousedown = (e: MouseEvent) => {
+            this.mouseButton("down");
+        };
+        canvas.onmouseup = (e: MouseEvent) => {
+            this.mouseButton("up");
+        };
+        canvas.onmousemove = (e: MouseEvent) => {
+            // account for gl-origin vs. screen origin:
+            this.mouseMove([-e.movementX, e.movementY]);
+        };
+        canvas.onwheel = (e: WheelEvent) => {
+            this.zoom(e.deltaY > 0 ? 1.1 : 0.9);
+        };
     }
 
     refreshScreen() {
+        if (!this.layer) return
 
-        // for each layer in order, draw it to the screen - 
-        this.layers.forEach((layer) => {
-            const image = layer.getImage();
-            // draw the image to the screen
+        this.regl.clear({ framebuffer: null, color: [0, 0, 0, 1], depth: 1 })
+        const src = this.layer.getRenderResults('prev');
+        this.imgRenderer({
+            box: Box2D.toFlatArray(src.bounds),
+            img: src.texture,
+            target: null,
+            view: Box2D.toFlatArray(this.camera.view)
         })
     }
 }
@@ -66,7 +142,7 @@ function demoTime() {
         extensions: ["ANGLE_instanced_arrays", "OES_texture_float", "WEBGL_color_buffer_float"],
     });
     const canvas: HTMLCanvasElement = regl._gl.canvas as HTMLCanvasElement;
-    theDemo = new Demo(canvas, regl);
+    theDemo = new Demo(canvas, regl, 'https://bkp-2d-visualizations-stage.s3.amazonaws.com/wmb_tenx_01172024_stage-20240128193624/488I12FURRB8ZY5KJ8T/ScatterBrain.json');
 }
 
 demoTime();
--- apps/layers/src/types.ts
+++ apps/layers/src/types.ts
@@ -4,13 +4,12 @@ import type REGL from "regl";
 
 // note: right now, all layers should be considered 2D, and WebGL only...
 export type Image = {
-    texture: REGL.Texture2D | REGL.Framebuffer2D
+    texture: REGL.Framebuffer2D
     bounds: box2D;
 }
-export interface Layer<T> {
-    getImage: () => Image;
-    update: (state: T, renderProgressCallback?: (s: NormalStatus) => void) => void;
-    cancel: () => void;
+
+
+export type BufferPair<T> = {
+    writeTo: T;
+    readFrom: T;
 }
-
-
