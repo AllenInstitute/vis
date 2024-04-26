1d6e650e920d0105b06051089e8594837595d9d9
noah <noah.shepard@alleninstitute.org>
2024-04-26 12:16:28 -0700

    play around with WW pool size, and fix a bug in which the optional render-working budget parameters were being ignored (for the slice renderer specifically)

--- apps/layers/src/demo.ts
+++ apps/layers/src/demo.ts
@@ -252,7 +252,7 @@ class Demo {
                     data: layer.data,
                     settings: {
                         ...settings,
-                        concurrentTasks: 20,
+                        concurrentTasks: 2,
                         renderer: renderers[layer.type],
                     }
                 })
