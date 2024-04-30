145b628d23e434e6a7ef80f604e8a4c5d400ab31
noah <noah.shepard@alleninstitute.org>
2024-04-30 15:22:56 -0700

    try out sds components for quick hacky ui fun - delete old ui code

--- apps/layers/src/app.tsx
+++ apps/layers/src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { SliceViewLayer } from './ui/layer-ui';
+import type { Demo } from './demo';
 
-export function AppUi() {
-  console.log('someone rendered me...');
-
-  return <div id="what">hey gusy</div>;
+export function AppUi(props: { demo: Demo }) {
+  return <SliceViewLayer demo={props.demo} />;
 }
