46cb6523d51eb81e321da2754d3763b760f00503
noah <nshepar@gmail.com>
2024-08-29 17:26:51 -0700

    start on constellation plots...

---
+++ examples/src/constellation/constellation.ts
@@ -0,0 +1,9 @@
+
+// a demo for playing with constellation plot ideas.
+
+
+// teh first ting to mess with is animating points through a heirarchy of positions...
+// a cell has a class, subclass, cluster, and super cluster
+// each value in those feature-types will now have a position associated with it - the centroid
+// of that thing in umap space.
+
