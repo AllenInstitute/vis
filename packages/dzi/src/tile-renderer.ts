c4f2fe8c8656a6c15bd073cf340f45cf868e9713
noah <noah.shepard@alleninstitute.org>
2024-09-16 10:08:39 -0700

    flip tiles right-side-up in shader, commit unit tests

--- packages/dzi/src/tile-renderer.ts
+++ packages/dzi/src/tile-renderer.ts
@@ -17,8 +17,9 @@ varying vec2 uv;
 void main(){
     uv = position;
     vec2 size = view.zw-view.xy;
-    vec2 tileSize = tile.zw-tile.xy;
-    vec2 tilePosition = (position * tileSize)+tile.xy;
+    vec2 tileSize = tile.zy-tile.xw;
+    vec2 ups = vec2(tile.x, 1.0-tile.y);
+    vec2 tilePosition = (position * tileSize)+ups.xy;
     vec2 pos =(tilePosition-view.xy)/size;
     // to clip space:
     pos = (pos*2.0)-1.0;
