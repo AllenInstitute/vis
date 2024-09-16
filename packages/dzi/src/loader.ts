b8e3ffd0980e9b21fb1050c1623194570d2851c5
noah <noah.shepard@alleninstitute.org>
2024-09-16 10:00:48 -0700

    fix a very confusing mistake in which rows and columns were swapped when building the tile URL

--- packages/dzi/src/loader.ts
+++ packages/dzi/src/loader.ts
@@ -9,7 +9,7 @@ export type DziImage = {
     // imagesUrl would be the path which contains all the files:
     // in this example:
     // http://blah.com/deepzoom_files/
-    format: 'jpeg' | 'png',
+    format: 'jpeg' | 'png' | 'jpg' | 'JPG' | 'PNG',
     overlap: number, // in pixels, ADDED every side of any given tile (for example, with overlap=1 and tilesize=256, you could see a jpeg of size 258x258). 
     // note that tiles on the edge wont have padding (on a per edge basis!)
     tileSize: number,
@@ -29,10 +29,10 @@ export type DziTile = {
     layer: number;
 }
 function tileUrl(dzi: DziImage, level: number, tile: TileIndex): string {
-    return `${dzi.imagesUrl}${level.toFixed(0)}/${tile.row.toFixed(0)}_${tile.col.toFixed(0)}.${dzi.format}`
+    return `${dzi.imagesUrl}${level.toFixed(0)}/${tile.col.toFixed(0)}_${tile.row.toFixed(0)}.${dzi.format}`
 }
 // some quick notes on this deepzoom image format:
-// 1. image / tile names are given by {row}_{column}.{format}
+// 1. image / tile names are given by {column}_{row}.{format}
 // 2. a layer (which may contain multiple tiles) is a folder
 // 2.1 that folder contains all the tiles for that layer.
 //     layer 0 should contain a single image, 0_0, which is a single pixel!
@@ -50,8 +50,34 @@ function tileUrl(dzi: DziImage, level: number, tile: TileIndex): string {
  */
 export function getVisibleTiles(dzi: DziImage, camera: { view: box2D, screenSize: vec2 }): DziTile[] {
     // TODO implement me
+    const layer = firstSuitableLayer(dzi.size.width, camera.screenSize[0]);
+    const layerResolution = imageSizeAtLayer(dzi, layer);
+    const availableTiles = tilesInLayer(dzi, layer);
+    // filter out tiles which are not in view
+    // note that the tile boxes are in pixels relative to the layer in which they reside
+    // the given view is assumed to be a parameter (in the space [0:1])
+    const tileBoxAsParameter = (tile: box2D) => Box2D.create(Vec2.div(tile.minCorner, layerResolution), Vec2.div(tile.maxCorner, layerResolution));
+    const tiles: DziTile[] = availableTiles.flatMap((row, rowIndex) => {
+        return row.map((tile, colIndex) => {
+            const index = { col: colIndex, row: rowIndex }
+            return {
+                index,
+                layer,
+                relativeLocation: tileBoxAsParameter(tile),
+                url: tileUrl(dzi, layer, index)
+            }
+        })
+    }).filter((t) => Box2D.intersection(t.relativeLocation, camera.view))
+    return tiles;
+    // return [{ url: tileUrl(dzi, 8, { row: 0, col: 0 }), index: { row: 0, col: 0 }, layer: 8, relativeLocation: Box2D.create([0, 0], [1, 1]) }]
+}
 
-    return [{ url: tileUrl(dzi, 8, { row: 0, col: 0 }), index: { row: 0, col: 0 }, layer: 8, relativeLocation: Box2D.create([0, 0], [1, 1]) }]
+// starting with the width of an image, and the width of the screen on which to display that image
+// return the highest-numbered (aka highest resolution) dzi-layer folder which has a size that would be lower than the screen resolution 
+export function firstSuitableLayer(imageWidth: number, screenWidth: number) {
+    const idealLayer = Math.ceil(Math.log2(screenWidth))
+    const biggestRealLayer = Math.ceil(Math.log2(imageWidth))
+    return Math.min(biggestRealLayer, idealLayer)
 }
 
 export function tileWithOverlap(total: number, step: number, overlap: number): Interval[] {
@@ -71,7 +97,7 @@ function boxFromRowCol(row: Interval, col: Interval) {
     return Box2D.create([col.min, row.min], [col.max, row.max])
 }
 export function imageSizeAtLayer(dzi: DziImage, layer: number) {
-    const { overlap, size, tileSize } = dzi;
+    const { size } = dzi;
     const layerMaxSize = 2 ** layer;
     let total: vec2 = [size.width, size.height];
     while (total[0] > layerMaxSize || total[1] > layerMaxSize) {
@@ -79,13 +105,13 @@ export function imageSizeAtLayer(dzi: DziImage, layer: number) {
     }
     return total;
 }
-export function tilesInLayer(dzi: DziImage, layer: number): box2D[] {
-    const { overlap, size, tileSize } = dzi;
+export function tilesInLayer(dzi: DziImage, layer: number): box2D[][] {
+    const { overlap, tileSize } = dzi;
     const layerMaxSize = 2 ** layer;
     // figure out the effective size of a layer by dividing the total size by 2 until its less than our layerMax
     // note: if this all feels weird, its because I can find no reference implementation or specification, its a bit of reverse engineering
     const total: vec2 = imageSizeAtLayer(dzi, layer)
     const rows = tileWithOverlap(Math.ceil(total[1]), tileSize, overlap);
     const cols = tileWithOverlap(Math.ceil(total[0]), tileSize, overlap);
-    return rows.flatMap(r => cols.map(c => boxFromRowCol(r, c)))
+    return rows.map(r => cols.map(c => boxFromRowCol(r, c)))
 }
