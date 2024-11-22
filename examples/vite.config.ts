7d3260a95e2bb1f0fc888fba425b22f7b276d0f0
Lane Sawyer <lane.sawyer@alleninstitute.org>
2024-11-22 10:18:34 -0800

    chore: Deploy example site

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+    // `base` enables GitHub Pages deployment to function
+    base: '/vis/',
     build: {
         rollupOptions: {
             input: {
