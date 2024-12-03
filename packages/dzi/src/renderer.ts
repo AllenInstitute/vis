f8966c7706c57889d2451b758d16f2194136624c
noah <noah.shepard@alleninstitute.org>
2024-12-03 15:25:59 -0800

    documentation of exported functions and types

--- packages/dzi/src/renderer.ts
+++ packages/dzi/src/renderer.ts
@@ -11,7 +11,13 @@ import { buildTileRenderer } from './tile-renderer';
 
 export type RenderSettings = {
     camera: {
+        /**
+         * a region of a dzi image, expressed as a relative parameter (eg. [0,0],[1,1] means the whole image)
+         */
         view: box2D;
+        /**
+         * the resolution of the output screen on which to project the region of source pixels given by view
+         */
         screenSize: vec2;
     };
 };
@@ -19,6 +25,12 @@ export type RenderSettings = {
 type GpuProps = {
     pixels: CachedTexture;
 };
+/**
+ * 
+ * @param regl a valid REGL context (https://github.com/regl-project/regl)
+ * @returns an object which can fetch tiles from a DeepZoomImage, determine the visibility of those tiles given a simple camera, and render said tiles
+ * using regl (which uses webGL)
+ */
 export function buildDziRenderer(regl: REGL.Regl): Renderer<DziImage, DziTile, RenderSettings, GpuProps> {
     const renderCmd = buildTileRenderer(regl, { enable: false });
     const fetchDziTile = (
@@ -45,7 +57,7 @@ export function buildDziRenderer(regl: REGL.Regl): Renderer<DziImage, DziTile, R
         };
     };
     return {
-        destroy: () => {}, // no private resources to destroy
+        destroy: () => { }, // no private resources to destroy
         cacheKey: (item, _requestKey, _data, _settings) => `${item.url}`,
         fetchItemContent: fetchDziTile,
         getVisibleItems: (dzi, settings) => {
@@ -68,7 +80,13 @@ export function buildDziRenderer(regl: REGL.Regl): Renderer<DziImage, DziTile, R
         },
     };
 }
-
+/**
+ * 
+ * @param regl  a valid REGL context (https://github.com/regl-project/regl)
+ * @returns a function which creates a "Frame" of actions. each action represents loading
+ * and subsequently rendering a tile of the image as requested via its configuration -
+ * @see RenderSettings
+ */
 export function buildAsyncDziRenderer(regl: REGL.Regl) {
     return buildAsyncRenderer(buildDziRenderer(regl));
 }
