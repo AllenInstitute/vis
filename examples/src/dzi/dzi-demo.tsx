11bce5e982c53ff7bcdd3b259aebde332764045e
Lane Sawyer <lane.sawyer@alleninstitute.org>
2025-02-10 14:16:31 -0800

    chore: Fix lints in examples [DT-7060]

--- examples/src/dzi/dzi-demo.tsx
+++ examples/src/dzi/dzi-demo.tsx
@@ -1,7 +1,7 @@
 import type { DziImage, DziRenderSettings } from '@alleninstitute/vis-dzi';
 import { Box2D, type box2D, type vec2 } from '@alleninstitute/vis-geometry';
 import { useEffect, useMemo, useRef, useState } from 'react';
-import React from 'react';
+import type React from 'react';
 import { pan, zoom } from '~/common/camera';
 import { RenderServerProvider } from '../common/react/render-server-provider';
 import { DziViewer } from './dzi-viewer';
