0b0d594a5a126b317de2d3ba4f1e433fccb13bea
noah <noah.shepard@alleninstitute.org>
2026-04-22 12:16:47 -0700

    switch to vite (library mode) because it supports typeGPU's wgsl transformer plugin...

---
+++ packages/scatterbrain/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path';
+import dts from 'vite-plugin-dts';
+import typegpuPlugin from 'unplugin-typegpu/vite';
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(import.meta.dirname, 'src/index.ts'),
+            formats: ['es'],
+            fileName: 'main',
+        },
+    },
+    resolve: {
+        alias: {
+            '@': resolve(import.meta.dirname, 'src'),
+        },
+    },
+    plugins: [
+        typegpuPlugin(),
+        dts({
+            rollupTypes: true,
+        }),
+    ],
+});
