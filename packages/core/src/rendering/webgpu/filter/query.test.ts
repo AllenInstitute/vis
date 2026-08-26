08fcff081525df063703e1b7ebb3c167c5536c06
noah <noah.shepard@alleninstitute.org>
2026-08-26 15:55:51 -0700

    test shader generation via simple snapshot

--- packages/core/src/rendering/webgpu/filter/query.test.ts
+++ packages/core/src/rendering/webgpu/filter/query.test.ts
@@ -74,4 +74,59 @@ describe('expression building', () => {
             rhs: 'stuff',
         });
     });
+    describe('aggregation shader', () => {
+        const tables = {
+            cells: { A: 'vec2f', B: 'u32' },
+            edges: { E: 'vec2u', str: 'f32' },
+        } as const;
+        const { column, table, clause, groupBy } = given(tables).from('edges');
+        test('sum edge strength, grouped by some categorical property on cells', () => {
+            const mod = groupBy(table('cells').at('E.x').dot('B'), table('cells').at('E.y').dot('B'))
+                .sum(column('str'), '$count', '$unused', '$unused')
+                .shader();
+            expect(mod.format).toEqual('rg32float')
+            const lines = mod.code.split('\n').map(s => s.trim()).filter(s => s !== '');
+            const expected = `struct VsIn {
+            @builtin(vertex_index) vIndex: u32,
+            // delightfully, we dont need instancing! this is because 1px dots are completely fine in this scenaro! yay!
+        };
+        struct VsOut {
+            @builtin(position) pos: vec4f,
+            @location(0) value:vec2f,
+        };
+        @group(0) @binding(0)
+        var<uniform> outputDimensions: vec2u;
+        
+   //cells
+  @group(1) @binding(1) var<storage,read> cells_A: array<vec2f>;
+  @group(1) @binding(2) var<storage,read> cells_B: array<u32>;
+   //edges
+  @group(1) @binding(3) var<storage,read> edges_E: array<vec2u>;
+  @group(1) @binding(4) var<storage,read> edges_str: array<f32>;
+        @vertex
+        fn vmain(v:VsIn)-> VsOut  {
+            var out:VsOut;
+            let element = v.vIndex;
+            let row = cells_B[edges_E[element].y];
+            let col = cells_B[edges_E[element].x];
+            let size = outputDimensions;
+            // convert the integer positions into output (clip) space:
+            let pos = (vec2f(vec2u(col,row))+vec2f(0.5,0.5))/vec2f(size);
+            // pos is now in unit space, relative to camera, coodinates at the center of pixels
+            let clip = (pos*2.0)-1.0;
+            // upside down please, to match texture memory origin, rather than 'screen origin'
+            out.pos = vec4f(clip*vec2f(1.0,-1.0),0.5,1.0);
+            // now gather the values that the blending-stage will aggregate:
+            out.value = vec2f(f32(edges_str[element]),f32(1));
+            return out;
+        }
+        @fragment
+        fn fmain(v:VsOut) ->@location(0) vec2f {
+            return v.value;
+        }`
+            expect(lines).toEqual(expected.split('\n').map(s => s.trim()).filter(s => s !== ''))
+        })
+
+
+    })
 });
--- packages/core/src/rendering/webgpu/filter/query.ts
+++ packages/core/src/rendering/webgpu/filter/query.ts
@@ -88,8 +88,8 @@ function compilePredicate<Params extends Record<string, string | number | number
 ) {
     return group instanceof AndGroup
         ? group.ands
-              .flatMap((c) => `(${c.predicates.flatMap((p) => toWgsl(p, 'element', uniName)).join(' || ')})`)
-              .join(' && ')
+            .flatMap((c) => `(${c.predicates.flatMap((p) => toWgsl(p, 'element', uniName)).join(' || ')})`)
+            .join(' && ')
         : group.predicates.flatMap((p) => toWgsl(p, 'element', uniName)).join(' || ');
 }
 function componentType(t: WgslType): ScalarType {
@@ -205,26 +205,26 @@ class Selection<Ts extends Tables, From extends keyof Ts> {
                 });
                 const bindings = indexed
                     ? (args as RunIndexedFilterArgs<Ts>).sets.map((s, i) => {
-                          return device.createBindGroup({
-                              layout: pipe.pipeline.getBindGroupLayout(1),
-                              entries: [
-                                  { binding: 0, resource: s.resultCounter },
-                                  { binding: 1, resource: s.results },
-                                  { binding: 2, resource: s.elements },
-                                  ...mapTablesToBindings(s.tables, safeLookups),
-                              ],
-                          });
-                      })
+                        return device.createBindGroup({
+                            layout: pipe.pipeline.getBindGroupLayout(1),
+                            entries: [
+                                { binding: 0, resource: s.resultCounter },
+                                { binding: 1, resource: s.results },
+                                { binding: 2, resource: s.elements },
+                                ...mapTablesToBindings(s.tables, safeLookups),
+                            ],
+                        });
+                    })
                     : args.sets.map((s, i) => {
-                          return device.createBindGroup({
-                              layout: pipe.pipeline.getBindGroupLayout(1),
-                              entries: [
-                                  { binding: 0, resource: s.resultCounter },
-                                  { binding: 1, resource: s.results },
-                                  ...mapTablesToBindings(s.tables, safeLookups),
-                              ],
-                          });
-                      });
+                        return device.createBindGroup({
+                            layout: pipe.pipeline.getBindGroupLayout(1),
+                            entries: [
+                                { binding: 0, resource: s.resultCounter },
+                                { binding: 1, resource: s.results },
+                                ...mapTablesToBindings(s.tables, safeLookups),
+                            ],
+                        });
+                    });
 
                 const bg0 = device.createBindGroup({
                     layout: pipe.pipeline.getBindGroupLayout(0),
@@ -234,9 +234,9 @@ class Selection<Ts extends Tables, From extends keyof Ts> {
                 const pass = enc.beginComputePass(
                     timestampWrites
                         ? {
-                              label,
-                              timestampWrites,
-                          }
+                            label,
+                            timestampWrites,
+                        }
                         : { label }
                 );
                 pass.setPipeline(pipe.pipeline);
@@ -454,20 +454,23 @@ export function given<Ts extends Tables>(tables: Ts) {
                 T extends ScalarType,
             > = From extends string
                 ? O extends string
-                    ? keyof Ts[From] extends string
-                        ? keyof Ts[O] extends string
-                            ? IndexExpr<O, string, T> | ColumnExpr<From, string, T>
-                            : never
-                        : never
-                    : never
+                ? keyof Ts[From] extends string
+                ? keyof Ts[O] extends string
+                ? IndexExpr<O, string, T> | ColumnExpr<From, string, T>
+                : never
+                : never
+                : never
                 : never;
 
             function groupBy<ColGroup extends keyof Ts, RowGroup extends keyof Ts>(
                 col: GroupSubject<Ts, From, ColGroup>,
                 row?: GroupSubject<Ts, From, RowGroup>
             ) {
+                function generateShader(conf: AggregationConfig) {
+                    return generateAggregationShader(tables, from as string, conf, col, row);
+                }
                 function buildAggregate(dev: GPUDevice, conf: AggregationConfig) {
-                    const shader = generateAggregationShader(tables, from as string, conf, col, row);
+                    const shader = generateShader(conf);
 
                     const { code, format, op } = shader;
                     const pipe = buildAggregationPipeline(dev, code, format, op, 'histogram');
@@ -494,6 +497,7 @@ export function given<Ts extends Tables>(tables: Ts) {
                         a: '$unused' | AggregationSubject<Ts, From, A, TT>
                     ) => {
                         return {
+                            shader: () => generateShader({ op: 'min', layout: [r, g, b, a] }),
                             build: (device: GPUDevice) => {
                                 return buildAggregate(device, { op: 'min', layout: [r, g, b, a] });
                             },
@@ -513,6 +517,7 @@ export function given<Ts extends Tables>(tables: Ts) {
                         a: '$unused' | AggregationSubject<Ts, From, A, TT>
                     ) => {
                         return {
+                            shader: () => generateShader({ op: 'max', layout: [r, g, b, a] }),
                             build: (device: GPUDevice) => {
                                 return buildAggregate(device, { op: 'max', layout: [r, g, b, a] });
                             },
@@ -532,6 +537,7 @@ export function given<Ts extends Tables>(tables: Ts) {
                         a: '$count' | '$unused' | AggregationSubject<Ts, From, A, TT>
                     ) => {
                         return {
+                            shader: () => generateShader({ op: 'sum', layout: [r, g, b, a] }),
                             build: (device: GPUDevice) => {
                                 return buildAggregate(device, { op: 'sum', layout: [r, g, b, a] });
                             },
